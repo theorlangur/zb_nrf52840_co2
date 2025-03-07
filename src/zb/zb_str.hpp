47069c7f27c176a86eaaabc7f9973278f61bad71
theorlangur <theorlangur@gmail.com>
2025-03-07 22:27:27 +0100

    copied ZbStr from esp code base

---
+++ src/zb/zb_str.hpp
@@ -0,0 +1,82 @@
+#ifndef ZB_STR_HPP
+#define ZB_STR_HPP
+
+#include "zb_types.hpp"
+#include <cstring>
+#include <string_view>
+#include <span>
+
+namespace zb
+{
+    struct ZigbeeStrView
+    {
+        char *pStr;
+
+        operator void*() { return pStr; }
+        uint8_t size() const { return pStr[0]; }
+        std::string_view sv() const { return {pStr + 1, pStr[0]}; }
+    };
+
+    struct ZigbeeStrRef
+    {
+        char sz;
+
+        operator void*() { return this; }
+        uint8_t size() const { return sz; }
+        std::string_view sv() const { return {&sz + 1, sz}; }
+    };
+
+    template<size_t N>
+    struct ZigbeeStrBuf: public ZigbeeStrRef
+    {
+        char data[N];
+    };
+
+    struct ZigbeeOctetRef
+    {
+        uint8_t sz;
+
+        operator void*() { return this; }
+        uint8_t size() const { return sz; }
+        std::span<const uint8_t> sv() const { return {&sz + 1, sz}; }
+    };
+
+    template<size_t N>
+    struct ZigbeeOctetBuf: public ZigbeeOctetRef
+    {
+        uint8_t data[N];
+    };
+
+    template<size_t N>
+    struct ZigbeeStr
+    {
+        char name[N];
+        operator void*() { return name; }
+        size_t size() const { return N - 1; }
+        std::string_view sv() const { return {name + 1, N - 1}; }
+        ZigbeeStrView zsv() const { return {name}; }
+        ZigbeeStrRef& zsv_ref() { return *(ZigbeeStrRef*)name; }
+
+        template<size_t M>
+        ZigbeeStr<N>& operator=(const char (&n)[M])
+        {
+            static_assert(M <= N, "String literal is too big");
+            name[0] = M - 1;
+            std::memcpy(name + 1, n, M - 1);
+            return *this;
+        }
+
+        static Type TypeId() { return Type::CharStr; }
+    };
+
+
+    template<size_t N>
+    constexpr ZigbeeStr<N> ZbStr(const char (&n)[N])
+    {
+        static_assert(N < 255, "String too long");
+        return [&]<size_t...idx>(std::index_sequence<idx...>){
+            return ZigbeeStr<N>{.name={N-1, n[idx]...}};
+        }(std::make_index_sequence<N-1>());
+    }
+}
+#endif
