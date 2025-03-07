8673e09fc5c8aebe334e032dcab859a735753396
theorlangur <theorlangur@gmail.com>
2025-03-07 21:47:56 +0100

    (feast of templates) worked on generalization of any struct description as zigbee cluster
    by definig zb::get_cluster_description for a struct, one can later
    generate at compile time an attribute list for a given actual object
    (static) with zb::get_attributes_from_cluster_struct
    
    Compared to macros:
    Cons:
     * it's much harder to see the generated attribute list (with
    macro-based you could let the compiler spit out the generated code or
    clangd can show you immediately)
    Pros:
     * once the description is done in once place, every usage in other
    places is automatically correct (also type correct)
     * it's easier to see ID, type for a given attribute than with macros

--- src/zb/zb_main.hpp
+++ src/zb/zb_main.hpp
@@ -15,7 +15,7 @@ extern "C"
 
 namespace zb
 {
-    enum class AttAccess: zb_uint8_t
+    enum class Access: zb_uint8_t
     {
         None = 0,
         Read = 0x01,
@@ -25,20 +25,31 @@ namespace zb
         Scene = 0x10,
         ManuSpec = 0x20,
         Internal = 0x40,
+
+        RW = Read | Write,
+        RWP = RW | Report,
+        RP = Read | Report,
+        RPS = RP | Report,
     };
+
+    constexpr Access operator|(Access a1, Access a2) { return Access(zb_uint8_t(a1) | zb_uint8_t(a2)); }
 
     template<class T>
     constexpr uint8_t TypeToTypeId()
     {
-        if constexpr (std::is_same_v<T,zb_uint8_t>) return ZB_ZCL_ATTR_TYPE_U8;
-        else if constexpr (std::is_same_v<T,zb_uint16_t>) return ZB_ZCL_ATTR_TYPE_U16;
-        else if constexpr (std::is_same_v<T,zb_uint32_t>) return ZB_ZCL_ATTR_TYPE_U32;
-        else if constexpr (std::is_same_v<T,zb_uint64_t>) return ZB_ZCL_ATTR_TYPE_U64;
-        else if constexpr (std::is_same_v<T,zb_int8_t>)   return ZB_ZCL_ATTR_TYPE_S8;
-        else if constexpr (std::is_same_v<T,zb_int16_t>)  return ZB_ZCL_ATTR_TYPE_S16;
-        else if constexpr (std::is_same_v<T,zb_int32_t>)  return ZB_ZCL_ATTR_TYPE_S32;
-        else if constexpr (std::is_same_v<T,zb_int64_t>)  return ZB_ZCL_ATTR_TYPE_S64;
-        else if constexpr (std::is_same_v<T,std::nullptr_t>)   return ZB_ZCL_ATTR_TYPE_NULL;
+        if constexpr (std::is_same_v<T,zb_uint8_t>)                return ZB_ZCL_ATTR_TYPE_U8;
+        else if constexpr (std::is_same_v<T,zb_uint16_t>)          return ZB_ZCL_ATTR_TYPE_U16;
+        else if constexpr (std::is_same_v<T,zb_uint32_t>)          return ZB_ZCL_ATTR_TYPE_U32;
+        else if constexpr (std::is_same_v<T,zb_uint64_t>)          return ZB_ZCL_ATTR_TYPE_U64;
+        else if constexpr (std::is_same_v<T,zb_int8_t>)            return ZB_ZCL_ATTR_TYPE_S8;
+        else if constexpr (std::is_same_v<T,zb_int16_t>)           return ZB_ZCL_ATTR_TYPE_S16;
+        else if constexpr (std::is_same_v<T,zb_int32_t>)           return ZB_ZCL_ATTR_TYPE_S32;
+        else if constexpr (std::is_same_v<T,zb_int64_t>)           return ZB_ZCL_ATTR_TYPE_S64;
+        else if constexpr (std::is_same_v<T,std::nullptr_t>)       return ZB_ZCL_ATTR_TYPE_NULL;
+        else if constexpr (std::is_enum_v<T> && sizeof(T) == 1)    return ZB_ZCL_ATTR_TYPE_8BIT_ENUM;
+        else if constexpr (std::is_enum_v<T> && sizeof(T) == 2)    return ZB_ZCL_ATTR_TYPE_16BIT_ENUM;
+        else if constexpr (std::is_same_v<T,float>)                return ZB_ZCL_ATTR_TYPE_SINGLE;
+        else if constexpr (std::is_same_v<T,double>)               return ZB_ZCL_ATTR_TYPE_DOUBLE;
         else if constexpr (requires { (zb_uint8_t)T::TypeId(); })  return T::TypeId();
         else 
             static_assert(sizeof(T) == 0, "Unknown type");
@@ -48,7 +59,7 @@ namespace zb
     inline constexpr zb_zcl_attr_t g_LastAttribute{
         .id = ZB_ZCL_NULL_ID,
         .type = ZB_ZCL_ATTR_TYPE_NULL,
-        .access = (zb_uint8_t)AttAccess::None,
+        .access = (zb_uint8_t)Access::None,
         .manuf_code = ZB_ZCL_NON_MANUFACTURER_SPECIFIC,
         .data_p = nullptr
     };
@@ -58,8 +69,9 @@ namespace zb
     {
         using simplified_tag = void;
         zb_uint16_t id;
-        AttAccess a;
+        Access a;
         T *pData;
+        zb_uint8_t type = TypeToTypeId<T>();
     };
 
     template<class T>
@@ -67,13 +79,12 @@ namespace zb
     {
         return {
             .id = d.id, 
-            .type = TypeToTypeId<T>(), 
+            .type = d.type, 
             .access = (uint8_t)d.a, 
             .manuf_code = ZB_ZCL_NON_MANUFACTURER_SPECIFIC, 
             .data_p = d.pData
         };
     }
-
 
     template<size_t N>
     struct TAttributeList
@@ -87,7 +98,7 @@ namespace zb
         template<class... T>
         constexpr TAttributeList(zb_uint16_t r, ADesc<T>... d):
             attributes{
-                AttrDesc(zb::ADesc{ .id = ZB_ZCL_ATTR_GLOBAL_CLUSTER_REVISION_ID, .a = AttAccess::Read, .pData = &rev }),
+                AttrDesc(zb::ADesc{ .id = ZB_ZCL_ATTR_GLOBAL_CLUSTER_REVISION_ID, .a = Access::Read, .pData = &rev }),
                 (AttrDesc(d), ...)
                 , g_LastAttribute
             },
@@ -106,6 +117,48 @@ namespace zb
     {
         return TAttributeList<sizeof...(T)>(r, d...);
     }
+
+    template<class T, class MemType>
+    using mem_attr_t = MemType T::*;
+
+    template<class T, class MemType>
+    struct cluster_mem_desc_t
+    {
+        mem_attr_t<T, MemType> m;
+        zb_uint16_t id;
+        Access a;
+        zb_uint8_t type = TypeToTypeId<MemType>();
+    };
+
+    template<zb_uint16_t rev, auto... ClusterMemDescriptions>
+    struct cluster_struct_desc_t
+    {
+        template<zb_uint16_t rev2, auto... ClusterMemDescriptions2>
+        friend constexpr auto operator+(cluster_struct_desc_t<rev, ClusterMemDescriptions...> lhs, cluster_struct_desc_t<rev2, ClusterMemDescriptions2...> rhs)
+        {
+            static_assert(rev == rev2, "Must be the same revision");
+            return cluster_struct_desc_t<
+                rev,
+                ClusterMemDescriptions...,
+                ClusterMemDescriptions2...
+                >{};
+        }
+    };
+
+    template<class T, class DestT, class MemType> requires std::is_base_of_v<DestT, T>
+    constexpr ADesc<MemType> cluster_mem_to_attr_desc(T& s, cluster_mem_desc_t<DestT,MemType> d) { return {.id = d.id, .a = d.a, .pData = &(s.*d.m), .type = d.type}; }
+
+    template<class T,zb_uint16_t rev, auto... ClusterMemDescriptions>
+    constexpr auto cluster_struct_to_attr_list(T &s, cluster_struct_desc_t<rev, ClusterMemDescriptions...>)
+    {
+        return MakeAttributeList(rev, cluster_mem_to_attr_desc(s, ClusterMemDescriptions)...);
+    }
+
+    template<class zb_struct>
+    constexpr auto get_cluster_description();
+
+    template<class ZbS>
+    constexpr auto get_attributes_from_cluster_struct(ZbS &s) { return cluster_struct_to_attr_list(s, get_cluster_description<ZbS>()); }
 }
 
 #endif
