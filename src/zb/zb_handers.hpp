5a271b125f1d69efe0253e124e62b6f9ce591b1e
theorlangur <theorlangur@gmail.com>
2025-03-30 16:08:30 +0200

    added typed wrappers for handlers

--- src/zb/zb_handers.hpp
+++ src/zb/zb_handers.hpp
@@ -4,6 +4,7 @@
 #include "zb_buf.hpp"
 #include "zb_desc_helper_types_ep.hpp"
 #include "lib_notification_node.hpp"
+#include "zb_str.hpp"
 
 namespace zb
 {
@@ -16,6 +17,98 @@ namespace zb
     {
         set_attr_value_handler_t handler;
     };
+
+    template<auto F>
+    struct typed_set_attr_value_handler
+    {
+        static constexpr bool value = false;
+        static_assert(sizeof(F) == 0, "Not supported signature");
+        void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam)
+        {
+        }
+    };
+
+    template<class T> using TypedSetHandlerV1 = void(*)(const T&);
+    template<class T> using TypedSetHandlerV2 = void(*)(const T&, zb_zcl_device_callback_param_t *pDevCBParam);
+    template<class T> using TypedSetHandlerV3 = void(*)(const T&, zb_zcl_device_callback_param_t *pDevCBParam, zb_zcl_set_attr_value_param_t *p);
+    template<class T> using TypedSetHandlerV1_Copy = void(*)(T);
+    template<class T> using TypedSetHandlerV2_Copy = void(*)(T, zb_zcl_device_callback_param_t *pDevCBParam);
+    template<class T> using TypedSetHandlerV3_Copy = void(*)(T, zb_zcl_device_callback_param_t *pDevCBParam, zb_zcl_set_attr_value_param_t *p);
+
+    template<class T>
+    T const& get_typed_data(zb_zcl_set_attr_value_param_t *p)
+    {
+        //account for zigbee str
+        if constexpr (std::is_same_v<T, ZigbeeStrRef>)
+            return *(T*)p->values.data_variable;
+        else if constexpr (sizeof(T) == 1)
+            return *(T*)&p->values.data8;
+        else if constexpr (sizeof(T) == 2)
+            return *(T*)&p->values.data16;
+        else if constexpr (sizeof(T) == 3)
+            return *(T*)&p->values.data24;
+        else if constexpr (sizeof(T) == 4)
+            return *(T*)&p->values.data32;
+        else if constexpr (sizeof(T) == 6)
+            return *(T*)&p->values.data48;
+        else if constexpr (sizeof(T) == 8)
+            return *(T*)&p->values.data_ieee;
+    }
+
+    template<class T, TypedSetHandlerV1<T> F>
+    struct typed_set_attr_value_handler<F>
+    {
+        static constexpr bool value = true;
+        static void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam) { F(get_typed_data<T>(p)); }
+    };
+
+    template<class T, TypedSetHandlerV1_Copy<T> F>
+    struct typed_set_attr_value_handler<F>
+    {
+        static constexpr bool value = true;
+        static void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam) { F(get_typed_data<T>(p)); }
+    };
+
+    template<class T, TypedSetHandlerV2<T> F>
+    struct typed_set_attr_value_handler<F>
+    {
+        static constexpr bool value = true;
+        static void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam) { F(get_typed_data<T>(p), pDevCBParam); }
+    };
+
+    template<class T, TypedSetHandlerV2_Copy<T> F>
+    struct typed_set_attr_value_handler<F>
+    {
+        static constexpr bool value = true;
+        static void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam) { F(get_typed_data<T>(p), pDevCBParam); }
+    };
+
+    template<class T, TypedSetHandlerV3<T> F>
+    struct typed_set_attr_value_handler<F>
+    {
+        static constexpr bool value = true;
+        static void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam) { F(get_typed_data<T>(p), pDevCBParam, p); }
+    };
+
+    template<class T, TypedSetHandlerV3_Copy<T> F>
+    struct typed_set_attr_value_handler<F>
+    {
+        static constexpr bool value = true;
+        static void handle(zb_zcl_set_attr_value_param_t *p, zb_zcl_device_callback_param_t *pDevCBParam) { F(get_typed_data<T>(p), pDevCBParam, p); }
+    };
+
+    template<auto F> 
+    struct to_handler_t;
+
+    template<auto F> requires typed_set_attr_value_handler<F>::value
+    struct to_handler_t<F>
+    {
+        static constexpr set_attr_value_handler_t value = typed_set_attr_value_handler<F>::handle;
+    };
+
+    template<auto F>
+    constexpr static auto to_handler_v = to_handler_t<F>::value;
+
 
     struct SetAttrValHandlingNode: GenericNotificationNode<SetAttrValHandlingNode>
     {
@@ -106,6 +199,120 @@ namespace zb
     {
         report_attr_handler_desc_t h;
     };
+
+    template<class T> requires (std::is_integral_v<T> && std::is_signed_v<T>)
+    const T& get_typed_data(zb_uint8_t attr_type, zb_uint8_t *value)
+    {
+        if constexpr(sizeof(T) == 1)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::S8:
+                case zb::Type::E8:
+                    return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }
+        else if constexpr(sizeof(T) == 2)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::S16:
+                case zb::Type::E16:
+                    return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }
+        else if constexpr(sizeof(T) == 4)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::S32: return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }
+        else if constexpr(sizeof(T) == 8)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::S64: return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }else
+        {
+            ZB_ASSERT(false);
+            zb_osif_abort();
+        }
+    }
+
+    template<class T> requires (std::is_integral_v<T> && std::is_unsigned_v<T>)
+    const T& get_typed_data(zb_uint8_t attr_type, zb_uint8_t *value)
+    {
+        if constexpr(sizeof(T) == 1)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::U8:
+                case zb::Type::E8:
+                    return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }
+        else if constexpr(sizeof(T) == 2)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::U16:
+                case zb::Type::E16:
+                    return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }
+        else if constexpr(sizeof(T) == 4)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::U32: return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }
+        else if constexpr(sizeof(T) == 8)
+        {
+            switch(zb::Type(attr_type))
+            {
+                case zb::Type::U64: return *(T*)value;
+                default:
+                    ZB_ASSERT(false);
+                    zb_osif_abort();
+                    break;
+            }
+        }else
+        {
+            ZB_ASSERT(false);
+            zb_osif_abort();
+        }
+    }
 
     template<report_attr_handler_desc_t... handlers>
     void tpl_report_cb(zb_zcl_addr_t *addr, zb_uint8_t ep, zb_uint16_t cluster_id, zb_uint16_t attr_id, zb_uint8_t attr_type, zb_uint8_t *value)
