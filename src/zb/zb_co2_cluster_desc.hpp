d1d67b9418cbe0ca856e3c50355a557e3d1014d3
theorlangur <theorlangur@gmail.com>
2025-05-03 17:12:17 +0200

    refactored cluster descriptor struct
    attributes are now grouped into own template struct
    commands are introduced and grouped into another template struct

--- src/zb/zb_co2_cluster_desc.hpp
+++ src/zb/zb_co2_cluster_desc.hpp
@@ -23,8 +23,10 @@ namespace zb
         using T = zb_zcl_co2_basic_t;
         return cluster_struct_desc_t<
             {.id = kZB_ZCL_CLUSTER_ID_CO2},
+            cluster_attributes_desc_t<
                 cluster_mem_desc_t{.m = &T::measured_value,.id = 0x0000, .a=Access::RP}
-            >{};
+            >{}
+        >{};
     }
 
     struct zb_zcl_co2_t: zb_zcl_co2_basic_t
@@ -39,9 +41,11 @@ namespace zb
         return get_cluster_description<zb_zcl_co2_basic_t>() 
             + cluster_struct_desc_t<
             {.id = kZB_ZCL_CLUSTER_ID_CO2},
+            cluster_attributes_desc_t<
                 cluster_mem_desc_t{.m = &T::min_measured_value,.id = 0x0001},
                 cluster_mem_desc_t{.m = &T::max_measured_value,.id = 0x0002}
-            >{};
+            >{}
+        >{};
     }
 
     struct zb_zcl_co2_ext_t: zb_zcl_co2_t
@@ -55,8 +59,10 @@ namespace zb
         return get_cluster_description<zb_zcl_co2_t>() 
             + cluster_struct_desc_t<
             {.id = kZB_ZCL_CLUSTER_ID_CO2},
+            cluster_attributes_desc_t<
                 cluster_mem_desc_t{.m = &T::tolerance,.id = 0x0003}
-            >{};
+            >{}
+        >{};
     }
 
 DEFINE_NULL_CLUSTER_INIT_FOR(kZB_ZCL_CLUSTER_ID_CO2);
--- src/zb/zb_desc_helper_types_cluster.hpp
+++ src/zb/zb_desc_helper_types_cluster.hpp
@@ -32,13 +32,6 @@ namespace zb
         uint8_t u8;
     };
 
-    struct request_args_t
-    {
-        uint8_t ep;
-        uint16_t profile_id;
-        AddrMode addr_mode;
-    };
-
 
     template<class T, class MemType>
     using mem_attr_t = MemType T::*;
@@ -79,6 +72,7 @@ namespace zb
         } 
     };
 
+    //helper for attributes
     template<auto memPtr, auto...ClusterMemDescriptions>
     struct find_cluster_mem_desc_t;
 
@@ -88,7 +82,7 @@ namespace zb
         static constexpr auto mem_desc() { return MemDesc; }
     };
 
-    template<auto memPtr, auto MemDesc, auto...ClusterMemDescriptions> requires (MemDesc.m != memPtr)
+    template<auto memPtr, auto MemDesc, auto...ClusterMemDescriptions>
     struct find_cluster_mem_desc_t<memPtr, MemDesc, ClusterMemDescriptions...>: find_cluster_mem_desc_t<memPtr, ClusterMemDescriptions...>
     {
     };
@@ -96,7 +90,34 @@ namespace zb
     template<auto memPtr>
     struct find_cluster_mem_desc_t<memPtr>
     {
-        static constexpr auto mem_desc() { static_assert(sizeof(memPtr) == 0, "Pointer to a member is not an attribute"); }
+        static constexpr auto cmd_desc() { static_assert(sizeof(memPtr) == 0, "Pointer to a member is not an attribte"); }
+    };
+
+    //helper for commands
+    template<zb_uint8_t cmd_id, zb_uint16_t manuf_code = ZB_ZCL_MANUF_CODE_INVALID>
+    struct cluster_cmd_desc_t;
+
+    template<auto MemPtr>
+    constexpr uint8_t CmdIdFromCmdMemPtr = mem_ptr_traits<decltype(MemPtr)>::MemberType::kCmdId;
+
+    template<auto MemPtr, auto...CmdMemPtrs>
+    struct find_cluster_cmd_desc_t;
+
+    template<auto MemPtr, auto CmdMemPtr, auto...CmdMemPtrs> requires (MemPtr == CmdMemPtr)
+    struct find_cluster_cmd_desc_t<MemPtr, CmdMemPtr, CmdMemPtrs...>
+    {
+        static constexpr auto cmd_desc() { return typename mem_ptr_traits<decltype(MemPtr)>::MemberType{}; }
+    };
+
+    template<auto MemPtr, auto CmdMemPtr, auto...CmdMemPtrs>
+    struct find_cluster_cmd_desc_t<MemPtr, CmdMemPtr, CmdMemPtrs...>: find_cluster_cmd_desc_t<MemPtr, CmdMemPtrs...>
+    {
+    };
+
+    template<auto MemPtr>
+    struct find_cluster_cmd_desc_t<MemPtr>
+    {
+        static constexpr auto cmd_desc() { static_assert(sizeof(MemPtr) == 0, "Pointer to a member is not a command"); }
     };
 
     struct cluster_info_t
@@ -112,8 +133,7 @@ namespace zb
     struct request_runtime_args_t
     {
         zb_addr_u dst_addr;
-        uint8_t dst_ep = 0xff;
-        zb_callback_t cb = nullptr;
+        uint8_t dst_ep;
 
         using PoolType = ObjectPool<request_runtime_args_t, 4>;
         static PoolType g_Pool;
@@ -121,66 +141,152 @@ namespace zb
     using RequestPtr = request_runtime_args_t::PoolType::Ptr<request_runtime_args_t::g_Pool>;
     inline constinit request_runtime_args_t::PoolType request_runtime_args_t::g_Pool{};
 
-    template<zb_uint8_t cmd_id>
+    struct request_args_t
+    {
+        uint8_t ep;
+        zb_callback_t cb = nullptr;
+        uint16_t profile_id = ZB_AF_HA_PROFILE_ID;
+    };
+
+    struct request_args_intern_t: request_args_t
+    {
+        AddrMode addr_mode;
+    };
+
+    //as NTTP to cluster description template type
+    template<zb_uint8_t cmd_id, zb_uint16_t manuf_code>
     struct cluster_cmd_desc_t
     {
+        static constexpr uint8_t kCmdId = cmd_id;
+            
         template<cluster_info_t i, request_args_t r>
-        void request(/*runtime arguments*/)
+        static void request()
         {
             request_runtime_args_t *pArgs = request_runtime_args_t::g_Pool.Acquire();
-            pArgs->cb = nullptr;
-            pArgs->dst_ep = 0;
-            //pArgs->dst_addr = 0;
-            zigbee_get_out_buf_delayed_ext(on_out_buf_ready<i, r>, (uint16_t)request_runtime_args_t::g_Pool.PtrToIdx(pArgs), 0);
+            zigbee_get_out_buf_delayed_ext(
+                    &on_out_buf_ready<i, {{.ep = r.ep, .cb = r.cb, .profile_id = r.profile_id}, AddrMode::NoAddr_NoEP}>
+                    , (uint16_t)request_runtime_args_t::g_Pool.PtrToIdx(pArgs), 0);
         }
 
         template<cluster_info_t i, request_args_t r>
+        static void request(uint16_t short_addr, uint8_t ep)
+        {
+            request_runtime_args_t *pArgs = request_runtime_args_t::g_Pool.Acquire();
+            pArgs->dst_ep = ep;
+            pArgs->dst_addr.addr_short = short_addr;
+            zigbee_get_out_buf_delayed_ext(
+                    &on_out_buf_ready<i, {{.ep = r.ep, .cb = r.cb, .profile_id = r.profile_id}, AddrMode::Dst16EP}>
+                    , (uint16_t)request_runtime_args_t::g_Pool.PtrToIdx(pArgs), 0);
+        }
+
+        template<cluster_info_t i, request_args_t r>
+        static void request(zb_ieee_addr_t ieee_addr, uint8_t ep)
+        {
+            request_runtime_args_t *pArgs = request_runtime_args_t::g_Pool.Acquire();
+            pArgs->dst_ep = ep;
+            std::memcpy(pArgs->dst_addr.addr_long, ieee_addr, sizeof(zb_ieee_addr_t));
+            zigbee_get_out_buf_delayed_ext(
+                    &on_out_buf_ready<i, {{.ep = r.ep, .cb = r.cb, .profile_id = r.profile_id}, AddrMode::Dst64EP}>
+                    , (uint16_t)request_runtime_args_t::g_Pool.PtrToIdx(pArgs), 0);
+        }
+
+        template<cluster_info_t i, request_args_t r>
+        static void request(uint16_t group_addr)
+        {
+            request_runtime_args_t *pArgs = request_runtime_args_t::g_Pool.Acquire();
+            pArgs->dst_addr.addr_short = group_addr;
+            zigbee_get_out_buf_delayed_ext(
+                    &on_out_buf_ready<i, {{.ep = r.ep, .cb = r.cb, .profile_id = r.profile_id}, AddrMode::Group_NoEP}>
+                    , (uint16_t)request_runtime_args_t::g_Pool.PtrToIdx(pArgs), 0);
+        }
+
+        template<cluster_info_t i, request_args_t r>
+        static void request(uint8_t bind_table_id)
+        {
+            request_runtime_args_t *pArgs = request_runtime_args_t::g_Pool.Acquire();
+            pArgs->dst_ep = bind_table_id;
+            zigbee_get_out_buf_delayed_ext(
+                    &on_out_buf_ready<i, {{.ep = r.ep, .cb = r.cb, .profile_id = r.profile_id}, AddrMode::EPAsBindTableId}>
+                    , (uint16_t)request_runtime_args_t::g_Pool.PtrToIdx(pArgs), 0);
+        }
+
+        template<cluster_info_t i, request_args_intern_t r>
         static void on_out_buf_ready(zb_bufid_t bufid, uint16_t poolIdx)
         {
             request_runtime_args_t *pArgs = request_runtime_args_t::g_Pool.IdxToPtr(poolIdx);
             RequestPtr raii(pArgs);
 
+            constexpr uint16_t manu_code = i.manuf_code != ZB_ZCL_MANUF_CODE_INVALID ? i.manuf_code : manuf_code;
+
             frame_ctl_t f{.f{
                 .cluster_specific = true, 
-                .manufacture_specific = i.manuf_code != ZB_ZCL_MANUF_CODE_INVALID
+                .manufacture_specific = manu_code != ZB_ZCL_MANUF_CODE_INVALID
                 , .direction = FrameDirection::ToServer
                 , .disable_default_response = false
             }};
             ZB_ZCL_GET_SEQ_NUM();
-            uint8_t* ptr = (uint8_t*)zb_zcl_start_command_header(bufid, f.u8, i.manuf_code, cmd_id, nullptr);
-            zb_ret_t ret = zb_zcl_finish_and_send_packet(bufid, ptr, &pArgs->dst_addr, (uint8_t)r.addr_mode, pArgs->dst_ep, r.ep, r.profile_id, i.id, pArgs->cb);
-            RET_OK == ret;
+            uint8_t* ptr = (uint8_t*)zb_zcl_start_command_header(bufid, f.u8, manu_code, cmd_id, nullptr);
+            zb_ret_t ret = zb_zcl_finish_and_send_packet(bufid, ptr, &pArgs->dst_addr, (uint8_t)r.addr_mode, pArgs->dst_ep, r.ep, r.profile_id, i.id, r.cb);
+            if (RET_OK != ret && r.cb)
+                r.cb(uint8_t(ret));
         }
     };
 
-    template<cluster_info_t ci, auto... ClusterMemDescriptions>
+    template<auto... attributeMemberDesc>
+    struct cluster_attributes_desc_t
+    {
+        static constexpr inline size_t count_members_with_access(Access a) { return ((size_t)attributeMemberDesc.has_access(a) + ... + 0); }
+        static constexpr inline size_t count_cvc_members() { return ((size_t)attributeMemberDesc.is_cvc() + ... + 0); }
+        template<auto memPtr>
+        static constexpr inline auto get_member_description() { return find_cluster_mem_desc_t<memPtr, attributeMemberDesc...>::mem_desc(); }
+
+        template<auto... attributeMemberDesc2>
+        friend constexpr auto operator+(cluster_attributes_desc_t<attributeMemberDesc...> lhs, cluster_attributes_desc_t<attributeMemberDesc2...> rhs)
+        {
+            return cluster_attributes_desc_t< attributeMemberDesc..., attributeMemberDesc2... >{};
+        }
+    };
+
+    template<auto... cmdMemberDesc>
+    struct cluster_commands_desc_t
+    {
+        template<auto memPtr>
+        static constexpr inline auto get_cmd_description() { return find_cluster_cmd_desc_t<memPtr, cmdMemberDesc...>::cmd_desc(); }
+
+        template<auto... cmdMemberDesc2>
+        friend constexpr auto operator+(cluster_commands_desc_t<cmdMemberDesc...> lhs, cluster_commands_desc_t<cmdMemberDesc2...> rhs)
+        {
+            return cluster_commands_desc_t< cmdMemberDesc..., cmdMemberDesc2... >{};
+        }
+    };
+
+    template<cluster_info_t ci, auto attributes = cluster_attributes_desc_t<>{}, auto cmds = cluster_commands_desc_t<>{}>//auto = cluster_attributes_desc_t
     struct cluster_struct_desc_t
     {
         static constexpr inline zb_uint16_t rev() { return ci.rev; }
         static constexpr inline auto info() { return ci; }
-        static constexpr inline size_t count_members_with_access(Access a) { return ((size_t)ClusterMemDescriptions.has_access(a) + ... + 0); }
-        static constexpr inline size_t count_cvc_members() { return ((size_t)ClusterMemDescriptions.is_cvc() + ... + 0); }
+        static constexpr inline size_t count_members_with_access(Access a) { return attributes.count_members_with_access(a); }
+        static constexpr inline size_t count_cvc_members() { return attributes.count_cvc_members(); }
 
         template<auto memPtr>
-        static constexpr inline auto get_member_description() { return find_cluster_mem_desc_t<memPtr, ClusterMemDescriptions...>::mem_desc(); }
+        static constexpr inline auto get_member_description() { return attributes.template get_member_description<memPtr>(); }
 
-        template<cluster_info_t ci2, auto... ClusterMemDescriptions2>
-        friend constexpr auto operator+(cluster_struct_desc_t<ci, ClusterMemDescriptions...> lhs, cluster_struct_desc_t<ci2, ClusterMemDescriptions2...> rhs)
+        template<auto memPtr>
+        static constexpr inline auto get_cmd_description() { return cmds.template get_cmd_description<memPtr>(); }
+
+        template<cluster_info_t ci2, cluster_attributes_desc_t attributes2, cluster_commands_desc_t cmds2>
+        friend constexpr auto operator+(cluster_struct_desc_t<ci, attributes, cmds> lhs, cluster_struct_desc_t<ci2, attributes2, cmds2> rhs)
         {
             static_assert(ci == ci2, "Must be the same revision");
-            return cluster_struct_desc_t<
-                ci,
-                ClusterMemDescriptions...,
-                ClusterMemDescriptions2...
-                >{};
+            return cluster_struct_desc_t< ci, attributes + attributes2, cmds + cmds2>{};
         }
     };
 
     template<class T, class DestT, class MemType> requires std::is_base_of_v<DestT, T>
     constexpr ADesc<MemType> cluster_mem_to_attr_desc(T& s, cluster_mem_desc_t<DestT,MemType> d) { return {.id = d.id, .a = d.a, .pData = &(s.*d.m), .type = d.type}; }
 
-    template<class T,cluster_info_t ci, auto... ClusterMemDescriptions>
-    constexpr auto cluster_struct_to_attr_list(T &s, cluster_struct_desc_t<ci, ClusterMemDescriptions...>)
+    template<class T,cluster_info_t ci, auto... ClusterMemDescriptions, cluster_attributes_desc_t<ClusterMemDescriptions...> attributes, cluster_commands_desc_t cmds>
+    constexpr auto cluster_struct_to_attr_list(T &s, cluster_struct_desc_t<ci, attributes, cmds>)
     {
         return MakeAttributeList(&s, cluster_mem_to_attr_desc(s, ClusterMemDescriptions)...);
     }
