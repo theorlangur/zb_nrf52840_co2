2def1f55fcb4485fba0863c38651e0426c09a389
theorlangur <theorlangur@gmail.com>
2025-05-11 18:08:53 +0200

    using new 3.0 toolchain + configured to use ncs-zigbee as addon (R23)

--- e.sh
+++ e.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-source /home/orlangur/ncs/toolchains/b77d8c1312/env.sh
+# source /home/orlangur/ncs/toolchains/b77d8c1312/env.sh
+source /home/orlangur/ncs/toolchains/7cbc0036f4/env.sh
 $*
